ea7c98e81a2e52f9bee686a1bc40ef50edfb4702
Felix Kipruto <kimosopfelix@gmail.com>
2022-11-29 13:49:06 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", function(){
+  let form = document.querySelector('form')
+  form.addEventListener("submit",function (e){
+    e.preventDefault();
+   
+    addList(e.target.new.value)
+    form.reset()
+  })
+})
+ 
+function addList(list){
+let li = document.createElement('li')
+
+let btnn = document.createElement('button')
+btnn.addEventListener('click',letDelete)
+btnn.textContent = '  delete'
+
+li.textContent = `${list}  `
+li.appendChild(btnn)
+document.querySelector('#tasks').appendChild(li)
+
+}
+
+function letDelete(e){
+
+  e.target.parentNode.remove()
+}
